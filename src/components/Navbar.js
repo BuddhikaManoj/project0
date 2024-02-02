89987a2b44a0dd68b21e7f0a49662896980ad4f5
buddy <manoiddamalgoda@gmail.com>
2024-02-02 11:27:28 +0530

    create Navbar
    nav bar was created successfully

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+      <nav>
+        <ul>
+            <li><a href="#">one</a></li>
+            <li><a href="#">two</a></li>
+            <li><a href="#">three</a></li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
