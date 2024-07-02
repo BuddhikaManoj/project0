c2c0cd83ffc7a2aa8c4b40db0f3f9e8e2b751aa9
buddy <141224046+BuddhikaManoj@users.noreply.github.com>
2024-07-03 01:12:38 +0530

    server side configurations done

---
+++ client/src/Pages/ContentNew.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const ContentNew = () => {
+    return (
+        <div>
+            <a href="#">Manoj lokka</a>
+        </div>
+    )
+};
--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import { Content } from "./Content";
 
 
 export default function Home() {
@@ -11,6 +12,7 @@ export default function Home() {
             <div style={{width:"100%",height:"90%"}}>
                 <h2>This is Home page</h2>
             </div>
+            <Content/>
         </div>
     )
  }
