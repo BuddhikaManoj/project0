97e5c225278148fd1851144ee2697189f5c5178c
buddy <141224046+BuddhikaManoj@users.noreply.github.com>
2024-07-05 06:58:27 +0530

    password encrypted using bcrypt

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,22 +1,25 @@
 import userModel from '../models/userModel.js';
+import bcrypt from "bcryptjs"
 
 const authentication = async (req,res) => {
-    const { username,password,roll } = req.body;
-
+    const { username,password,role } = req.body;
+console.log(username,password,role);
     try{
         if (!username || !password) {
         return res.status(400).json({message: "Something is missing"});
         }
-        else{
+        
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
         const newUser = new userModel({
             username,
-            password,
-            roll
+            password : hashedPassword,
+            role,
         });
         await newUser.save();
         return res.status(200).json({message: "User registration is successfull"});
     }
-    }
+
     catch(error) {
         return res.status(500).json({message:"Error occured while saving data"});
     }
@@ -32,7 +35,8 @@ const loginData = async (req,res) => {
     if(!oldUser){
         return res .status(400).json({message:"Invalid username or password"});
     }
-    if(oldUser.password===password){
+    const isPassowrdCorrect = await bcrypt.compare(password,oldUser.password);
+    if(isPassowrdCorrect){
         return res .status(200).json({message:"Successfully logged in", data:oldUser});
     }
 }
